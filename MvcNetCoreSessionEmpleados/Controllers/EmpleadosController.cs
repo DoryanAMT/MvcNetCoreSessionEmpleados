a8ec69b0937263d6e67745314d20724adef7acbf
DoryanAlexandro <doryanamt@gmail.com>
2025-02-25 10:52:00 +0100

    2 version carrito de compra

--- MvcNetCoreSessionEmpleados/Controllers/EmpleadosController.cs
+++ MvcNetCoreSessionEmpleados/Controllers/EmpleadosController.cs
@@ -12,6 +12,46 @@ namespace MvcNetCoreSessionEmpleados.Controllers
         {
             this.repo = repo;
         }
+
+        public async Task<IActionResult> SessionEmpleados
+            (int? idEmpleado)
+        {
+            if (idEmpleado != null)
+            {
+                //  BUSCAMOS AL EMPLEADO
+                Empleado empleado = await this.repo.FindEmpleadoAsync(idEmpleado.Value);
+                //  PARA PODER ALMACENARLO
+                List<Empleado> empleadosList;
+                //  DEBEMOS PREGUNTAR SI TENEMOS ALMACENADOS UN CONJUNTO DE EMPLEDOS
+                //  ALMACENAMOS EN SESION
+                if (HttpContext.Session.GetObject<List<Empleado>>("EMPLEADOS") != null)
+                {
+                    //  RECUPERAMOS LOS EMPLEADOS QUE TENGAMOS EN SESSION
+                    empleadosList = HttpContext.Session.GetObject<List<Empleado>>("EMPLEADOS");
+                }
+                else
+                {
+                    //  SI NO EXISTE INSTANCIAMOS LA COLECCION
+                    empleadosList = new List<Empleado>();
+                }
+                //  ALMACENAMOS EL EMPLEADO DENTRO DE NUESTRA COLECCION
+                empleadosList.Add(empleado);
+                //  GUARDAMOS EN SESSION LA COLECCION CON EL NUEVO EMPLEADO
+                HttpContext.Session.SetObject("EMPLEADOS", empleadosList);
+                //  ENVIAMOS UN MENSAJE PARA MOSTRAR EL OBJETO LIST<EMPLEADO> DE LA SESSION
+                ViewData["MENSAJE"] = "Empleado" + empleado.Apellido +
+                    " almacenado correctamente.";
+            }
+            List<Empleado> empleados =
+                await this.repo.GetEmpleadosAsync();
+            return View(empleados);
+        }
+
+        public IActionResult EmpleadosAlmacenados()
+        {
+            return View();
+        }
+
         public async Task<IActionResult> SessionSalarios
             (int? salario)
         {
